0e757874c35b9b121231479dd6d922261bcde82e
Marcus Voß <m.voss@laposte.net>
2025-05-25 17:37:38 +0200

    update

--- src/Hardware/Vehicle.cpp
+++ src/Hardware/Vehicle.cpp
@@ -10,9 +10,11 @@ constexpr int PIN_MOTOR_LIFT_A = 0;
 constexpr int PIN_MOTOR_LIFT_B = 1;
 constexpr int PIN_MOTOR_1_A = 7;
 constexpr int PIN_MOTOR_1_B = 5;
+constexpr int PIN_MOTOR_2_A = 6;
+constexpr int PIN_MOTOR_2_B = 8;
 
-constexpr int STEERING_SERVO_MIN_MS = 1215;
-constexpr int STEERING_SERVO_MAX_MS = 2025;
+constexpr int STEERING_SERVO_MIN_MS = 1315;
+constexpr int STEERING_SERVO_MAX_MS = 1925;
 constexpr int LIFT_SERVO_MIN_MS = 1000;
 constexpr int LIFT_SERVO_MAX_MS = 1600;
 
@@ -30,19 +32,19 @@ void setupVehicle()
     liftServo.attach(PIN_LIFT_SERVO, LIFT_SERVO_MIN_MS, LIFT_SERVO_MAX_MS);
     steeringServo.attach(PIN_STEERING_SERVO, STEERING_SERVO_MIN_MS, STEERING_SERVO_MAX_MS);
 
-    // Initialize the motor lift
-    ledcAttach(PIN_MOTOR_LIFT_A, 5000, 8);
-    ledcAttach(PIN_MOTOR_LIFT_B, 5000, 8);
+    // Initialize drv8833
     ledcAttach(PIN_MOTOR_1_A, 5000, 8);
     ledcAttach(PIN_MOTOR_1_B, 5000, 8);
+    ledcAttach(PIN_MOTOR_2_A, 5000, 8);
+    ledcAttach(PIN_MOTOR_2_B, 5000, 8);
+    pinMode(PIN_MOTOR_LIFT_A, OUTPUT);
+    pinMode(PIN_MOTOR_LIFT_B, OUTPUT);
 }
 
 void setLiftMotorSpeed(int16_t speed)
 {
-    uint8_t dutyCycle = map(abs(speed), 0, std::numeric_limits<int16_t>::max(), 0, 255);
-    dutyCycle = ((1 << 8) - 1) - dutyCycle; // Invert the duty cycle for slow decay mode (active braking)
-    ledcWrite(PIN_MOTOR_LIFT_A, speed > 0 ? dutyCycle : 255);
-    ledcWrite(PIN_MOTOR_LIFT_B, speed < 0 ? dutyCycle : 255);
+    digitalWrite(PIN_MOTOR_LIFT_A, speed < 0 ? HIGH : LOW);
+    digitalWrite(PIN_MOTOR_LIFT_B, speed > 0 ? HIGH : LOW);
 }
 
 void setThrottle(int16_t speed)
@@ -50,6 +52,8 @@ void setThrottle(int16_t speed)
     uint8_t dutyCycle = map(abs(speed), 0, std::numeric_limits<int16_t>::max(), 0, 255);
     ledcWrite(PIN_MOTOR_1_A, speed > 0 ? dutyCycle : 0);
     ledcWrite(PIN_MOTOR_1_B, speed < 0 ? dutyCycle : 0);
+    ledcWrite(PIN_MOTOR_2_A, speed > 0 ? dutyCycle : 0);
+    ledcWrite(PIN_MOTOR_2_B, speed < 0 ? dutyCycle : 0);
 }
 
 void controlVehicle(const RemoteControlState &state)
